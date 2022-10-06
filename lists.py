b2a2cc815edcd5e7d5afad353e2b53be008eb4f5
TizianaAndreaOrlando <81337862+TizianaAndreaOrlando@users.noreply.github.com>
2022-10-06 15:55:22 -0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,8 @@
+"""Defining the lists"""
+
+carbrand = ['Mercedes Benz', 'BMW', 'Audi',
+            'Maserati', 'Nio', 'Tesla']
+
+
+location = ['United States', 'China', 'Brasil',
+            'Canada', 'Australia', 'Italy', 'Spain']
