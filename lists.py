b63e86833aad1c04f8f5d65b0b316cb9a966b0b9
TizianaAndreaOrlando <81337862+TizianaAndreaOrlando@users.noreply.github.com>
2022-10-06 15:54:46 -0300

    Delete lists.py

--- lists.py
+++
@@ -1,8 +0,0 @@
-"""Defining the lists"""
-
-carbrand = ['Mercedes Benz', 'BMW', 'Audi',
-            'Maserati', 'Nio', 'Tesla']
-
-
-location = ['United States', 'China', 'Brasil',
-            'Canada', 'Australia', 'Italy', 'Spain']
